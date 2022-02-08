6ff4379cf439b23ace11d8f6decd3bff786aa7f3
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-02-08 18:01:47 +0530

    XOR question

--- Bit Manipulation/Missing Number.java
+++ Bit Manipulation/Missing Number.java
@@ -3,7 +3,7 @@ class Solution {
         int ans = 0;
         
         //if we take the contents of array and their indices all values as a list 
-        // then there will be pair for all elements except the one that is missing
+        //then there will be pair for all elements except the one that is missing
         //XOR will cancel out all the pairs leaving behind the one missing
         for(int i=0; i<nums.length; i++){
             ans = ans ^ i;
