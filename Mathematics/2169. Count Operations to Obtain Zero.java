51b9facfaf41608c125446c000fb3ac2f249c8da
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-02-14 20:00:20 +0530

    count operations 0(iterative & 1 line-EUCLIDIAN)

--- Mathematics/2169. Count Operations to Obtain Zero.java
+++ Mathematics/2169. Count Operations to Obtain Zero.java
@@ -4,6 +4,17 @@ class Solution {
         // and after that n1 would be equal n1 % n2.
 
         // This is, pretty much, the Euclidean Algorithm. The complexity of this algorithm is O(log(min(a, b)).
+
+
+        //ITERATIVE APPROACH
+        // int res = 0;
+        // while (min(a, b) > 0) {
+        //     if (a > b)
+        //         swap(a, b);
+        //     res += b / a;
+        //     b %= a;
+        // }
+        // return res;
         
         return a * b == 0 ? 0 : a > b ? countOperations(b, a) : b / a + countOperations(a, b % a);
     }
