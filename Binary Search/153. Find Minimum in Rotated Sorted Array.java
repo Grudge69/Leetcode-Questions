3a4b46dbd76a8ae3296b4bbc8b5d1c9c3ffa2def
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-02-15 19:11:29 +0530

    find min in rot sorted arr

--- Binary Search/153. Find Minimum in Rotated Sorted Array.java
+++ Binary Search/153. Find Minimum in Rotated Sorted Array.java
@@ -19,11 +19,11 @@ class Solution {
             int mid = start + (end - start) / 2;
             // 4 cases over here
             if (mid < end && arr[mid] > arr[mid + 1]) {
-                // instead of returning pivot = mid, we return the next element which is the                        minimum
+                // instead of returning pivot = mid, we return the next element which is the minimum
                 return mid+1;
             }
             if (mid > start && arr[mid] < arr[mid - 1]) {
-                // instead of returning pivot = mid-1, we return the next element which is the                      minimum
+                // instead of returning pivot = mid-1, we return the next element which is the minimum
                 return mid;
             }
             if (arr[mid] <= arr[start]) {
