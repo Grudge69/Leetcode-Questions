64c8e6bcddd0faf5259188c124983839c2a5ec9e
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-02-21 13:01:46 +0530

    majority element

---
+++ Arrays and ArrayLists/169. Majority Element.java
@@ -0,0 +1,46 @@
+                     //QUESTION DESCRIPTION
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -231 <= nums[i] <= 231 - 1
+
+                     //SOLUTION 
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int val = nums[0], count = 0;
+        for(int num: nums){
+            if(num == val){
+                count++;
+            }else{
+                count = 1;
+                val = num;
+            }
+            
+            if(count>nums.length/2) return val;
+            
+        }
+        
+        return val;
+    }
+}
