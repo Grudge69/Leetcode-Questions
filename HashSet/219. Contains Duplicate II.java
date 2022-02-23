5565d269eb1e50eda98f6b9a71780d7fbbc73d3e
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-02-23 21:18:15 +0530

    contains duplicate

---
+++ HashSet/219. Contains Duplicate II.java
@@ -0,0 +1,43 @@
+//QUESTION DESCRIPTION
+
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+
+//SOLUTION(using SET)
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                // when pointer travels beyond k distance start removing elements from set that
+                // lie k dist behind i
+                // this is because we have to maintain absolute k distance
+                set.remove(nums[i - k - 1]);
+            }
+
+            if (!set.add(nums[i])) {
+                // set.add() adds a particular element only if it doesn't already exist in it
+                // we check if set is not allowing to add nums[i], this means it is duplicate
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
