03f71cd2063a2405cb6a02ea0628b31f4661dbaa
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-04 20:00:01 +0530

    fibonacci recursive solution

---
+++ Recursion/509. Fibonacci Number.java
@@ -0,0 +1,12 @@
+// Link: https://leetcode.com/problems/fibonacci-number/
+
+// SOLUTION
+
+// Recursive
+
+class Solution {
+    public int fib(int n) {
+        if(n==1 || n==0) return n;
+        return fib(n-1) + fib(n-2);
+    }
+}
