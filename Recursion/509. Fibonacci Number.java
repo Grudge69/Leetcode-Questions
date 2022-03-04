fcab999fbbaa579ea085a95335a0ef28f7b62ef3
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-04 20:04:17 +0530

    fibonacci recursive+iterative solution

--- Recursion/509. Fibonacci Number.java
+++ Recursion/509. Fibonacci Number.java
@@ -10,3 +10,20 @@ class Solution {
         return fib(n-1) + fib(n-2);
     }
 }
+
+// Iterative 100% faster
+
+class Solution {
+    public int fib(int n) {
+        int a=0, b=1, c=0;
+        if(n==0 || n==1) return n;
+        
+        for(int i=0; i<n-1; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        
+        return c;
+    }
+}
