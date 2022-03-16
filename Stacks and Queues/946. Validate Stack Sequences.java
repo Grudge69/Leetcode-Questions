02e75ec446ca4052cf704197768fe316b72c51b6
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-16 13:03:23 +0530

    validate stack O(1) space sol added

--- Stacks and Queues/946. Validate Stack Sequences.java
+++ Stacks and Queues/946. Validate Stack Sequences.java
@@ -18,3 +18,21 @@ class Solution {
         return st.isEmpty(); // check if stack is empty return true else false
     }
 }
+
+// Using PUSHED array as stack O(1), not extra space
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int i = 0; // Intialise one pointer pointing on pushed array
+        int j = 0; // Intialise one pointer pointing on popped array
+        
+        for(int val : pushed){
+            pushed[i++] = val; // using pushed as the stack.
+            while(i > 0 && pushed[i - 1] == popped[j]){ // pushed[i - 1] values equal to popped[j];
+                i--; // decrement i
+                j++; // increment j
+            }
+        }
+        return i == 0; // Since pushed is a permutation of popped so at the end we are supposed to be left with an empty stack
+    }
+}
