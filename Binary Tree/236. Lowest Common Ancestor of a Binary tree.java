3ae77bf3739dca541dd0c3923098084245ef1a7e
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-17 14:29:10 +0530

    lca bin tree

---
+++ Binary Tree/236. Lowest Common Ancestor of a Binary tree.java
@@ -0,0 +1,41 @@
+// Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+// Solution: DFS
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            //no node exists
+            return null;
+        }
+        
+        if(root == p || root == q){
+            //if root is one of the nodes to be found then root is LCA
+            return root;
+        }
+        
+        //Get left and right's answers 
+        TreeNode leftAns = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightAns = lowestCommonAncestor(root.right, p, q);
+        
+        if(leftAns!=null && rightAns!=null){
+            //if left and right have p & q in them then root is LCA
+            return root;
+        }else if(rightAns==null){
+            //p & q both are present in left tree, then left child is the LCA
+            return leftAns;
+        }
+        
+        //p & q both are present in right tree, then right child is the LCA
+        return rightAns;
+    }
+}
