1d779a9618dedfacc8c1cacb689bf34b7c6b735f
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-19 20:11:20 +0530

    2 pts 0ms sol added

--- HashMap & HashSet/1. Two Sum.java
+++ HashMap & HashSet/1. Two Sum.java
@@ -70,3 +70,22 @@ public class Solution {
         return null;
     }
 }
+
+
+//2 pointer approach BEST
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int forwardCounter =1;
+        while(forwardCounter < nums.length)
+        {
+            for(int i=0 ; (i + forwardCounter) < nums.length; i++)
+            {
+                if((nums[i] + nums[(i+forwardCounter)]) == target)
+                    return new int[]{i,(i+forwardCounter)};
+            }
+            forwardCounter++;
+        }
+      return null;  
+    }
+}
