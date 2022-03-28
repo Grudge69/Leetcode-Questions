0b69c3029ca56d248eebd1e3bc90728d6a924762
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-29 00:20:34 +0530

    with and without creating BST physically

--- Binary Search Tree/Array to BST.java
+++ Binary Search Tree/Array to BST.java
@@ -1,6 +1,6 @@
 // Link: https://practice.geeksforgeeks.org/problems/array-to-bst4443/1#
 
-// Solution
+// Solution : BY CREATING NODE CLASS
 
 // { Driver Code Starts
 //Initial Template for Java
@@ -96,5 +96,74 @@ class Solution
         root.right = helper(arr, mid+1, end);
         
         return root;
+    }
+}
+
+
+//////////////////////// WITHOUT CREATING NODE CLASS ////////////////////////
+
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] nums = new int[n];
+            String[] S = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++){
+                nums[i] = Integer.parseInt(S[i]);
+            }
+            Solution obj = new Solution();
+            int[] ans = obj.sortedArrayToBST(nums);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] sortedArrayToBST(int[] nums)
+    {
+        // Code here 
+        // to contain preorder of BST
+        int[] ans = new int[nums.length];
+        
+        helper(nums, 0, nums.length-1, ans);
+        
+        return ans;
+    }
+    
+    //index for position of element to be placed in our preorder array
+    int idx = 0;
+    //helper function processes the construction of BST
+    private void helper(int[] arr, int start, int end, int[] ans){
+        if(start > end){
+            //just like binary search STOP end start idx exceeds end idx
+            return;
+        }
+        
+        //while processing in preOrder add it in our array also
+        int mid = start + (end - start)/2;
+        ans[idx++] = arr[mid];
+        
+        helper(arr, start, mid-1, ans);
+        helper(arr, mid+1, end, ans);
+        
+        return;
     }
 }
