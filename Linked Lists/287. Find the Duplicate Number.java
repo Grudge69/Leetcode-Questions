0a00459d7179d72391353476a0ed02a75684f006
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-29 19:18:54 +0530

    floyd cycle detection soln added

--- Linked Lists/287. Find the Duplicate Number.java
+++ Linked Lists/287. Find the Duplicate Number.java
@@ -1,22 +1,32 @@
+// Link: https://leetcode.com/problems/find-the-duplicate-number/
+
+// SOLUTION: FLOYD'S CYCLE DETECTION
 class Solution {
     public int findDuplicate(int[] nums) {
-        // use floyd's cycle detection
+        //slow ptr moves 1 step while fast ptr moves 2 steps at a time,
+        //if they meet then there is a cycle
+        //the starting point of that cycle is the no. which is repeating
         int slow = nums[0], fast = nums[0];
-        while (true) {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-            if (slow == fast) {
+        
+        //DETECTING CYCLE -> use floyd's cycle detection
+        while(true){
+            slow = nums[slow]; //moves by 1 step
+            fast = nums[nums[fast]]; //moves by 2 steps
+            
+            if(slow == fast){//cycle detected
                 break;
             }
         }
-
-        // reset slow
-        fast = nums[0];
-        while (slow != fast) {
+        
+        //DETECTING STARTING POINT OF THE CYCLE
+        //reset slow or fast anyone and then move both by 1 step, wherever they meet is the starting point of the cycle
+        slow = nums[0];
+        
+        while(slow!=fast){
             slow = nums[slow];
             fast = nums[fast];
         }
-
-        return slow;
+        
+        return slow; // return fast; //both are pointing at starting pt of cycle
     }
 }
