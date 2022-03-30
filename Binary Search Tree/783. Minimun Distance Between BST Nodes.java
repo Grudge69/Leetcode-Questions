89f756592ee1ab6b0eae0be1bc14debc4e296777
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-30 19:53:12 +0530

    dfs INORDER is sorted BST

---
+++ Binary Search Tree/783. Minimun Distance Between BST Nodes.java
@@ -0,0 +1,47 @@
+// Link: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+// Solution: DFS -> Inorder is sorted Property of BST
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int minDiff = Integer.MAX_VALUE;
+    TreeNode prev = null;
+    
+    public int minDiffInBST(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        
+        inorder(root);
+        
+        return minDiff;
+    }
+    
+    private void inorder(TreeNode root){
+        if(root == null) return;
+        
+        inorder(root.left);
+        
+        if(prev!=null){
+            minDiff = Math.min(minDiff, Math.abs(prev.val-root.val));
+        }
+        
+        prev = root;
+        
+        inorder(root.right);
+    }
+}
