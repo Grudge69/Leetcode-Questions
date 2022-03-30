4b3214ba80d918f4dc5c6c817f108279c03c5dcf
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-30 19:41:13 +0530

    binary search

--- Binary Search/74. Search a 2D Matrix.java
+++ Binary Search/74. Search a 2D Matrix.java
@@ -1,5 +1,10 @@
+// Link: https://leetcode.com/problems/search-a-2d-matrix/
+
+// SOLUTION: Binary Search
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        //SINCE THE MATRIX IS SORTED, WE CAN USE BINARY SEARCH
         int r = 0;
         int c = matrix[0].length - 1;
 
@@ -8,16 +13,16 @@ class Solution {
             // if target found then return true result or the row, col indices
             if (matrix[r][c] == target) {
                 return true;
-                // return new int[]{r, c};
+                // return new int[]{r, c}; //you can return the row, col also like this
             }
-
-            if (matrix[r][c] < target) {
-                // target lies on the right side
-                // discard this col and all others on left of it
-                r++;
-            } else {
+            
+            if (target > matrix[r][c]) {
                 // target lies on down side
                 // discard this row and all others above it
+                r++;
+            } else {
+                // target lies on the left side
+                // discard this col and all others on left of it
                 c--;
             }
         }
