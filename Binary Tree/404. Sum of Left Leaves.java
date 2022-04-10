bb02b9f212c1f2d4d11f9da08e368c43615ed8ac
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-04-10 13:21:22 +0530

    dfs soln

---
+++ Binary Tree/404. Sum of Left Leaves.java
@@ -0,0 +1,40 @@
+// Link: https://leetcode.com/problems/sum-of-left-leaves/
+
+// Solution: DFS
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+        //return 0 if no node exists
+        if(root == null) return 0;
+        
+        //calculates sum of left leaf nodes
+        int sum = 0;
+        
+        //if left node exists, then check if it is a leaf or not => add its value to sum if left node is a leaf
+        if(root.left!=null && (root.left.left == null && root.left.right == null)){
+            sum += root.left.val;
+        }
+        
+        //add the sum of remaining left leaf nodes from left and right subtree
+        sum += sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        
+        //return final calculated sum
+        return sum;
+    }
+}
