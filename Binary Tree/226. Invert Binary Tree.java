243a121e2de370cbd85139fbd54a2885fd50a83a
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-04-11 11:38:19 +0530

    dfs recursion

---
+++ Binary Tree/226. Invert Binary Tree.java
@@ -0,0 +1,35 @@
+// Link: https://leetcode.com/problems/invert-binary-tree/
+
+// Solution: DFS
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        
+        //get left and right subtree inverted
+        TreeNode leftSubTreeInverted = invertTree(root.left);
+        TreeNode rightSubTreeInverted = invertTree(root.right);
+        
+        //join left subtree to right of root and right subtree to left of root
+        root.left = rightSubTreeInverted;
+        root.right = leftSubTreeInverted;
+        
+        //return the tree
+        return root;
+    }
+}
