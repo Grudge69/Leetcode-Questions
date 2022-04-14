29b9427ecd58b521962db0ce6bd5c9664095ef3f
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-04-14 13:29:41 +0530

    sqr matrix optimize only

---
+++ Arrays and ArrayLists/867. Transpose Matrix.java
@@ -0,0 +1,50 @@
+// Link: 
+
+// Solution: Little optimize for square matrix
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        if(matrix.length == matrix[0].length){
+            //square matrix make changes in same matrix
+            for(int i=0; i<matrix.length; i++){
+                for(int j=0; j<=i; j++){
+                    swap(matrix, i, j, j, i);
+                }
+            }
+            
+            return matrix;
+        }
+        
+        //non square, take new matrix
+        int[][] res = new int[matrix[0].length][matrix.length];
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                res[j][i] = matrix[i][j];
+            }
+        }
+        
+        return res;
+    }
+    
+    public void swap(int[][] matrix, int i, int j, int k, int l){
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[k][l];
+        matrix[k][l] = temp;
+    }
+}
+
+// taking separate matrix for result
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int[][] res = new int[matrix[0].length][matrix.length];
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                res[j][i] = matrix[i][j];
+            }
+        }
+        
+        return res;
+    }
+    
+}
