1d5c6c477cd21b8699acad84f65f2790e109ae4a
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-04-22 18:55:53 +0530

    brute + hashmap + hashset soln

--- HashMap & HashSet/523. Continuous Subarray Sum.java
+++ HashMap & HashSet/523. Continuous Subarray Sum.java
@@ -2,6 +2,35 @@
 
 // Solution: 
 
+// Brute Force O(N*N) time, O(1) space
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int sum = nums[nums.length-1];
+        
+        for (int i = nums.length-2 ; i>= 0 ; i--) {
+            sum += nums[i];
+            
+            if (nums[i] == 0 && nums[i+1] == 0) 
+                return true;
+            
+           if (sum >= k || sum == 0) {
+               int internalSum = nums[i];
+               
+               for (int j = i+1 ; j < nums.length ; j++) {
+                    internalSum += nums[j];
+                   
+                   if (internalSum%k == 0) {
+                       return true;
+                   }
+               }
+           }     
+        }
+        
+        return false;
+    }
+}
+
 //Using HASHMAP O(N) time, O(K) space => in map we are storing remainders and remainders range from 0,k
 
 class Solution {
