74332652059ba9bf7e29dab9d2bb3056d40ecd0b
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-09 07:11:30 +0530

    2 methods

--- Recursion/17. Letter Combinations of a Phone Number.java
+++ Recursion/17. Letter Combinations of a Phone Number.java
@@ -65,3 +65,36 @@ class Solution {
         return list;
     }
 }
+
+// Alternate Solution
+
+//other solution using array storing the mapping of no. to digits
+class Solution {
+    public static String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty()) return new ArrayList<>();
+        
+        return helper("", digits);
+    }
+    
+    public List<String> helper(String p, String up) {
+        if(up.isEmpty()) {
+            List<String> base = new ArrayList<>();
+            base.add(p);
+            return base;
+        }
+        
+        int firstNum = up.charAt(0) - '0';
+        
+        String values = map[firstNum];
+        
+        List<String> ans = new ArrayList<>();
+        
+        for(char val: values.toCharArray()) {
+            ans.addAll(helper(p+val, up.substring(1)));
+        }
+        
+        return ans;
+    }
+}
