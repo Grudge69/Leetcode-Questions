c78a27b63aa3ca3b51c86a57176b0bfba50268b9
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-12 20:52:23 +0530

    dfs + bfs

--- Binary Tree/965. Univalued Binary Tree.java
+++ Binary Tree/965. Univalued Binary Tree.java
@@ -1,6 +1,8 @@
 // Link: https://leetcode.com/problems/univalued-binary-tree/
 
-// Solution: DFS
+// Solution
+
+// DFS
 
 /**
  * Definition for a binary tree node.
@@ -27,5 +29,41 @@ class Solution {
         if(root == null) return true;
         
         return (val == root.val) && helper(root.left, val) && helper(root.right, val);
+    }
+}
+
+// BFS
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        if(root == null) return false;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        int val = root.val;
+        q.add(root);
+        
+        while(q.size()!=0) {
+            TreeNode curr = q.remove();
+            if(curr.val != val) return false;
+            
+            if(curr.left!=null) q.add(curr.left);
+            if(curr.right!=null) q.add(curr.right);
+        }
+        
+        return true;
     }
 }
