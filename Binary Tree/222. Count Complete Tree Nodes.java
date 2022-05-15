ef0346bbd7f9dd01daa4889b9d58c029d357a4fc
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-15 22:07:09 +0530

    brute + optimized approach

--- Binary Tree/222. Count Complete Tree Nodes.java
+++ Binary Tree/222. Count Complete Tree Nodes.java
@@ -2,6 +2,7 @@
 
 // Solution
 
+// BRUTE APPROACHES
 //Approach 1
 
 class Solution {
@@ -25,5 +26,39 @@ class Solution {
     public int countNodes(TreeNode root) {
         if(root == null) return 0;
         return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
+
+// Optimized Approach
+
+class Solution {
+    public int countNodes(TreeNode root) {
+       return helper(root);
+    }
+
+    public int helper(TreeNode root) {
+        if(root==null) return 0;
+        
+        int l=0;
+        int r=0;
+        
+        //find lowest level on left side
+        TreeNode current=root;
+        while(current!=null) {
+            l++;
+            current=current.left;
+        }
+        
+        //find lowest level on right side
+        current=root;
+        while(current!=null) {
+            r++;
+            current=current.right;
+        }
+        
+        //if a complete binary tree(then left and right ends are on same level) exists then nodes = 2^L-1
+        if(l==r) return (int)Math.pow(2,l)-1;
+        //otherwise traverse on left and right side
+        else return 1+helper(root.left)+helper(root.right);
     }
 }
