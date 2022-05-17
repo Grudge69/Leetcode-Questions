f051d791f7a0a84014524f2ba9999376658f67ed
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-17 12:24:23 +0530

    dfs

---
+++ Binary Tree/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.java
@@ -0,0 +1,30 @@
+// Link: https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+
+// Solution
+
+// DFS
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if(original == null) return null;
+        if(original == target) {
+            return cloned;
+        }
+        
+        TreeNode leftAns = getTargetCopy(original.left, cloned.left, target);
+        TreeNode rightAns = getTargetCopy(original.right, cloned.right, target);
+        
+        if(leftAns != null) return leftAns;
+        return rightAns;
+    }
+}
