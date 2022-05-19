fb4a46353759325516cf99c1ff1d842c68333574
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-19 22:19:17 +0530

    morris traversal + dfs

--- Binary Tree/94. Binary Tree Inorder Traversal.java
+++ Binary Tree/94. Binary Tree Inorder Traversal.java
@@ -1,18 +1,9 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Link: https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+// Solution
+
+// Using DFS: TC: O(N), Space: O(ht of tree)
+
 class Solution {
     List<Integer> ans = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
@@ -21,6 +12,51 @@ class Solution {
         inorderTraversal(root.left);
         ans.add(root.val);
         inorderTraversal(root.right);
+        return ans;
+    }
+}
+
+// Using Morris Traversal: TC: O(N), Space: O(1)
+
+class Solution {
+    
+    public List<Integer> inorderTraversal(TreeNode curr) {
+        List<Integer> ans = new ArrayList<>();
+        if(curr == null)
+            return ans;
+        
+        //work till curr exists
+        while(curr != null) {
+            //if left child doesn't exist
+            if(curr.left==null) {
+                //print curr and go to right
+                ans.add(curr.val);
+                curr = curr.right;
+            } 
+            //if left child exists
+            else {
+                //find inOrder predecessor -> left's rightMost
+                TreeNode iop = curr.left;
+                //till iop's right points to curr or null
+                while(iop.right!=null && iop.right!=curr) {
+                    iop = iop.right;
+                }
+                //left is unprocessed
+                if(iop.right == null) {
+                    //make thread and go to left
+                    iop.right = curr;
+                    curr = curr.left;
+                } 
+                //left is processed
+                else {
+                    //break thread, print and go to right
+                    iop.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        
         return ans;
     }
 }
