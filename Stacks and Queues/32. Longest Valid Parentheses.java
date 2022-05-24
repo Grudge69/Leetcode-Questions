df424ac025c4263dad2d21762692689eba2f9de3
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-24 11:55:47 +0530

    stack soln

---
+++ Stacks and Queues/32. Longest Valid Parentheses.java
@@ -0,0 +1,27 @@
+// Link:
+
+// Solution
+
+// Using Stack
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stk = new Stack<>();
+        stk.push(-1);
+        int ans = 0;
+        
+        for(int i=0; i<s.length(); i++) {
+            //finding longest difference b/w indices of '(' ')' pair when ')' is encountered
+            if(s.charAt(i)==')' && stk.size()>1 && s.charAt(stk.peek())=='(') {
+                stk.pop();
+                ans = Math.max(ans, i-stk.peek());
+            } 
+            //push index of '('
+            else {
+                stk.push(i);
+            }
+        }
+        
+        return ans;
+    }
+}
