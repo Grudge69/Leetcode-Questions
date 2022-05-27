a4041990f5facf350945a11b0620a786d16db53b
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-28 00:13:49 +0530

    morris inorder + merge sort

--- Binary Tree/1382. Balance a Binary Search Tree.java
+++ Binary Tree/1382. Balance a Binary Search Tree.java
@@ -2,21 +2,6 @@
 
 // Solution: Morris Inorder
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode balanceBST(TreeNode root) {
         if(root == null || (root.left==null && root.right==null)) {
@@ -76,5 +61,34 @@ class Solution {
                 }
             }
         }
+    }
+}
+
+// Merge Sort
+
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList();
+        inOrder(root,list);
+        return makeTree(list,0,list.size() - 1);
+    }
+    public TreeNode makeTree(ArrayList<Integer> list, int low, int high){
+        if(low > high){
+            return null;
+        }   
+        int middle = low + (high - low)/2;
+        TreeNode node = new TreeNode(list.get(middle));
+        node.left= makeTree(list,low,middle - 1);
+        node.right= makeTree(list,middle + 1,high);
+        return node;
+    }
+    
+    public void inOrder(TreeNode root,ArrayList<Integer> list){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left,list);
+        list.add(root.val);
+        inOrder(root.right,list);
     }
 }
