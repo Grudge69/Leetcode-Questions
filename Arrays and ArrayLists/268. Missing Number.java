319f7bdc1ead1a4e25017ef3d4b5f1eff10e5410
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-28 11:05:24 +0530

    2 approaches

--- Arrays and ArrayLists/268. Missing Number.java
+++ Arrays and ArrayLists/268. Missing Number.java
@@ -1,9 +1,12 @@
+// Link: https://leetcode.com/problems/missing-number/
+
+// Approach 1 - Bit Manipulation
 class Solution {
     public int missingNumber(int[] nums) {
         int ans = 0;
         
         //if we take the contents of array and their indices all values as a list 
-        // then there will be pair for all elements except the one that is missing
+        //then there will be pair for all elements except the one that is missing
         //XOR will cancel out all the pairs leaving behind the one missing
         for(int i=0; i<nums.length; i++){
             ans = ans ^ i;
@@ -13,5 +16,22 @@ class Solution {
         ans = ans ^ nums.length;
         
         return ans;
+    }
+}
+
+// Approach 2 - Mathematics Formula Sum 0-n
+
+class Solution {
+    // formula sum - sum of val in array = missing number
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int num: nums) {
+            sum += num;
+        }
+        
+        int n = nums.length;
+        int formulaSum = n*(n+1)/2;
+        
+        return formulaSum - sum;
     }
 }
