6c06a6b5ab8a7fcd8adc079f0457006507b20945
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-28 11:04:59 +0530

    2 approaches

--- Bit Manipulation/268. Missing Number.java
+++ Bit Manipulation/268. Missing Number.java
@@ -1,3 +1,6 @@
+// Link: https://leetcode.com/problems/missing-number/
+
+// Approach 1 - Bit Manipulation
 class Solution {
     public int missingNumber(int[] nums) {
         int ans = 0;
@@ -13,5 +16,22 @@ class Solution {
         ans = ans ^ nums.length;
         
         return ans;
+    }
+}
+
+// Approach 2 - Mathematics Formula Sum 0-n
+
+class Solution {
+    // formula sum - sum of val in array = missing number
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int num: nums) {
+            sum += num;
+        }
+        
+        int n = nums.length;
+        int formulaSum = n*(n+1)/2;
+        
+        return formulaSum - sum;
     }
 }
