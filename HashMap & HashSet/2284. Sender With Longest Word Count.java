85a499566760edb0beb3810f12270fe69d3a621e
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-28 23:30:45 +0530

    hashmap soln

---
+++ HashMap & HashSet/2284. Sender With Longest Word Count.java
@@ -0,0 +1,55 @@
+// Link: https://leetcode.com/problems/sender-with-largest-word-count/
+
+// Solution
+
+// Using Hashmap
+
+class Solution {
+    
+    public String largestWordCount(String[] messages, String[] senders) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        int n = messages.length;
+        
+        //store name -> word count
+        for(int i=0; i<n; i++) {
+            String sender = senders[i];
+            String message = messages[i];
+            
+            //count no. of words
+            // int words = message.split(" ").length;
+            int words = 1;
+            for(char c: message.toCharArray()) {
+                if(c==' ') words++;
+            }
+            
+            if(!map.containsKey(sender)) {
+                map.put(sender, words);
+            } else {
+                map.put(sender, map.get(sender)+words);
+            }
+        }
+        
+        //get highest word count
+        int max = 0;
+        for(String key: map.keySet()) {
+            int val = map.get(key);
+            if(val>max) {
+                max = val;
+            }
+        }
+        
+        //Store the names with same max word count 
+        ArrayList<String> names = new ArrayList();
+        for(String key: map.keySet()) {
+            int val = map.get(key);
+            if(val == max) {
+                names.add(key);
+            }
+        }
+        
+        //sort the array to get lexicographically highest
+        Collections.sort(names);
+        return names.get(names.size()-1);
+    }
+}
