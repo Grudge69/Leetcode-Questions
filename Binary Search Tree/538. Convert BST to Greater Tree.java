707c738d3aa028545c2aed851984a0110f8b7fea
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-06-08 22:56:55 +0530

    reverse inorder + alt approach

--- Binary Search Tree/538. Convert BST to Greater Tree.java
+++ Binary Search Tree/538. Convert BST to Greater Tree.java
@@ -2,21 +2,6 @@
 
 // Solution: reverse inorder of BST is DECREASING order
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode convertBST(TreeNode root) {
         if(root == null){
@@ -48,3 +33,25 @@ class Solution {
         reverseInorder(root.left);
     }
 }
+
+// Alternate Approach
+
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        rec(root, 0);
+        return root ;
+    }
+    
+    public int rec(TreeNode node , int prevSum){
+        //add the value of rightSubTree to node
+        //pass this as prevSum to add to leftSubTree
+        //return the value of leftSubTree
+        if(node == null)
+            return prevSum ;
+        int rightValue = rec(node.right, prevSum);
+        node.val += rightValue ;
+        int leftValue = rec(node.left, node.val);
+        return leftValue ;
+    }
+}
+
