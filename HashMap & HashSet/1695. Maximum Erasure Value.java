fe9678d5b495ddb7a9afad8d5156fbdc89a04c47
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-06-12 06:41:33 +0530

    2 approaches

---
+++ HashMap & HashSet/1695. Maximum Erasure Value.java
@@ -0,0 +1,56 @@
+// Link:
+
+// Solution
+
+// Using HashSet
+
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int first=0, second=0;
+        int sum = 0, maxSum = 0;
+        while(second<nums.length) {
+            if(set.add(nums[second])) {
+                //unique element
+                sum += nums[second];
+                second++;
+            } else {
+                //non unique
+                set.remove(nums[first]);
+                sum -= nums[first];
+                first++;
+            }
+            
+            maxSum = Math.max(maxSum, sum);
+        }
+        
+        return maxSum;
+    }
+}
+
+// Using Visited Array
+
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        boolean[] visited = new boolean[10001];
+        int first=0, second=0;
+        int sum = 0, maxSum = 0;
+        while(second<nums.length) {
+            if(!visited[nums[second]]) {
+                //unique element
+                visited[nums[second]] = true;
+                sum += nums[second];
+                second++;
+            } else {
+                //non unique
+                visited[nums[first]] = false;//discard first value
+                sum -= nums[first];
+                first++;
+            }
+            
+            maxSum = Math.max(maxSum, sum);
+        }
+        
+        return maxSum;
+    }
+}
