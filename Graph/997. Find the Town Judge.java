ac515c2ac2c4365c1400117d1c21904a82ce444b
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-07-11 17:04:16 +0530

    InDegree, OutDegree

---
+++ Graph/997. Find the Town Judge.java
@@ -0,0 +1,25 @@
+// Link: https://leetcode.com/problems/find-the-town-judge/
+
+// Solution: InDegree, OutDegree
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] inDegree = new int[n+1];
+        int[] outDegree = new int[n+1];
+        
+        for(int[] t: trust) {
+            // [1, 2] means 1 --> 2, this signifies outgoing edge from 1 and incoming edge to 2 
+            outDegree[t[0]]++;
+            inDegree[t[1]]++;
+        }
+        
+        for(int i=1; i<=n; i++) {
+            if(outDegree[i] == 0 && inDegree[i] == n-1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
+
