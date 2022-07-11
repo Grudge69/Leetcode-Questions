c7f3d9bff863cde93346e6d3970bd96e5da4f3e8
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-07-11 17:05:29 +0530

    InDegree, OutDegree

--- Graph/997. Find the Town Judge.java
+++ Graph/997. Find the Town Judge.java
@@ -13,6 +13,7 @@ class Solution {
             inDegree[t[1]]++;
         }
         
+        // Check if outgoing edges = 0 and incoming edges = n-1
         for(int i=1; i<=n; i++) {
             if(outDegree[i] == 0 && inDegree[i] == n-1) {
                 return i;
@@ -22,4 +23,3 @@ class Solution {
         return -1;
     }
 }
-
