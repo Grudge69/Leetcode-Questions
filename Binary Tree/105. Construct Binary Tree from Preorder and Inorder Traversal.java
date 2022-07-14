582e7703fa3b94a948906821411f3f8f74171c8e
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-07-14 08:40:31 +0530

    brute + hashmap optimization

--- Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal.java
+++ Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -4,21 +4,6 @@
 
 // Un Optimized using Loop for searching
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         TreeNode root = construct(0, preorder.length-1, 0, inorder.length-1, preorder, inorder);
@@ -51,49 +36,36 @@ class Solution {
 
 //Optimized Using HashMap
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer, Integer> inordMap = new HashMap();
+        HashMap<Integer, Integer> inMap = new HashMap<>();
+        //inorder map: elem -> idx
         for(int i=0; i<inorder.length; i++) {
-            int val = inorder[i];
-            inordMap.put(val, i);
+            inMap.put(inorder[i], i);
         }
-        TreeNode root = construct(0, preorder.length-1, 0, inorder.length-1, preorder, inordMap);
-        return root;
+        
+        return helper(0, preorder.length-1, 0, inorder.length-1, preorder, inMap);
     }
     
-    private TreeNode construct(int pre_lo, int pre_hi, int in_lo, int in_hi, int[] pre, HashMap<Integer, Integer> inordMap) {
-        //base case
-        if(pre_lo > pre_hi) {
+    private TreeNode helper(int preLo, int preHi, int inLo, int inHi, int[] pre, HashMap<Integer, Integer> inMap) {
+        //out of bounds
+        if(preLo > preHi) {
             return null;
         }
         
-        TreeNode node = new TreeNode();
+        //preorder's left most is the root
+        TreeNode root = new TreeNode(pre[preLo]);
         
-        node.val = pre[pre_lo];
+        //get position of curr root in inorder
+        int inPos = inMap.get(pre[preLo]);
         
-        int idx = inordMap.get(pre[pre_lo]);
+        //get no. of elements in the left subtree which will be told by the inorder array as => Left Node Right
+        int elemToLeft = inPos - inLo;
         
-        int lhs = idx - in_lo;
+        //get the left and right subtree of root built by the helper()
+        root.left = helper(preLo+1, preLo+elemToLeft, inLo, inPos-1, pre, inMap);
+        root.right = helper(preLo+elemToLeft+1, preHi, inPos+1, inHi, pre, inMap);
         
-        node.left = construct(pre_lo+1, pre_lo+lhs, in_lo, idx-1, pre, inordMap);
-        node.right = construct(pre_lo+lhs+1, pre_hi, idx+1, in_hi, pre, inordMap);
-        
-        return node;
+        return root;
     }
 }
